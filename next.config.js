8dfcbbd52a2053ddaa0d43d0350cf3c6a32e3ec5
Ahmed Mehmood Faraz <96286002+ahmed-atrule@users.noreply.github.com>
2023-11-02 17:17:36 +0500

    change

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         {
+  //           key: "Cache-Control",
+  //           value: "public, max-age=31536000, immutable",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
   // output: "export",
   // trailingSlash: true,
   compress: true,
